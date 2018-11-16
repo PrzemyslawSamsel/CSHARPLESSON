df482a81d06620ac590fc01ec5d6d7dc9835399c
Przemek <sp39427@zut.edu.pl>
2018-11-16 19:07:47 +0100

    Added new project intro_1

---
+++ intro_1/intro_1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro_1
+{
+    class Class1
+    {
+        public static void my_method1()
+        {
+            Console.WriteLine("This is method1 from class1");
+        }
+    }
+}
---
+++ intro_1/intro_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a string");
+
+            Class1.my_method1();
+
+            
+
+                
+
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
